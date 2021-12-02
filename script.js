686b863eed6790ab2ed783e3407e23737e538283
Carlos Rafael <carlos.raafael@engenharia.ufjf.br>
2021-12-02 20:41:54 -0300

    Requisito 18

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const email = document.getElementsByTagName('input')[0];
 const password = document.getElementsByTagName('input')[1];
 const btnForm = document.getElementsByTagName('button')[0];
+const checkBox = document.getElementById('agreement');
+const btnSend = document.getElementById('submit-btn');
+
 function formValidation() {
   const emailContent = email.value;
   const passwordContent = password.value;
@@ -11,3 +14,12 @@ function formValidation() {
   }
 }
 btnForm.addEventListener('click', formValidation);
+
+function enableSend() {
+  if (checkBox.checked) {
+    btnSend.disabled = false;
+  } else {
+    btnSend.disabled = true;
+  }
+}
+checkBox.addEventListener('click', enableSend);
